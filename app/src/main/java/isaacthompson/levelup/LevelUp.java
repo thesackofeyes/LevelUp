4f5138a9b168a84aef843ea33793f66cc0d2e4c5
isaac.thompson <iamthompson96@gmail.com>
2017-02-01 15:33:13 +0000

    initial commit

---
+++ app/src/main/java/isaacthompson/levelup/LevelUp.java
@@ -0,0 +1,13 @@
+package isaacthompson.levelup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LevelUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_up);
+    }
+}
